e1b05f95b2a4ea29aea3a63c2aa05da32b0407d8
Elyas <el.idrissi_elyas@icloud.com>
2021-06-22 19:52:19 +0200

    inital implementation of class 'Node'

--- include/Node/Node.h
+++ include/Node/Node.h
@@ -5,9 +5,67 @@
 #ifndef SYNKER_NODE_H
 #define SYNKER_NODE_H
 
+#include <string>
+#include <vector>
+#include <cstddef>
+
+#include <boost/filesystem.hpp>
+
+/**
+ * class 'Node' represents an element of the filesystem,
+ * which can be a file or a directory (these two only as for now)
+ */
 class Node
 {
+	public:
 
+	enum Type {NODE, FILE, DIRECTORY};
+	enum PathType {DEFAULT, ABSOLUTE, RELATIVE};
+	enum PermissionType {READ, WRITE, EXECUTE};
+	enum PermissionOwner {USER, GROUP, OTHER};
+
+	Node() = default;
+	explicit Node(Type type);
+	Node(Type type, const boost::filesystem::path & path);
+	virtual ~Node() = default;
+
+	static Type get_guessed_type(const boost::filesystem::path & path);
+
+	boost::filesystem::path get_path(PathType path_type = DEFAULT);
+	boost::filesystem::path get_absolute_path();
+	boost::filesystem::path get_relative_path();
+	void set_path(const boost::filesystem::path & path);
+
+	static bool get_is_exists(const boost::filesystem::path & path);
+	bool get_is_exists();
+
+	std::string get_filename();
+	std::string get_extention();
+
+	const Type & get_type();
+	std::string get_type_str();
+
+	virtual size_t get_size() = 0;
+
+	time_t get_last_write_date_time();
+	time_t get_creation_date_time();
+	std::string get_last_write_date_str();
+	std::string get_creation_date_str();
+
+	bool get_has_permissions(PermissionOwner owner, const std::vector<PermissionType> & types);
+
+	bool get_is_hidden();
+
+	virtual std::string to_string();
+
+protected:
+
+	void set_type(const Node::Type & type);
+
+private:
+
+	Type type = NODE;
+	boost::filesystem::path path;
 };
 
 #endif //SYNKER_NODE_H
--- src/Node/Node.cpp
+++ src/Node/Node.cpp
@@ -3,3 +3,152 @@
 //
 
 #include "Node/Node.h"
+
+Node::Node(Node::Type type)
+{
+	set_type(type);
+}
+
+Node::Node(Node::Type type, const boost::filesystem::path & path) : Node(type)
+{
+	set_path(path);
+}
+
+bool Node::get_is_exists(const boost::filesystem::path & path)
+{
+	return boost::filesystem::exists(path);
+}
+
+bool Node::get_is_exists()
+{
+	return Node::get_is_exists(get_path());
+}
+
+Node::Type Node::get_guessed_type(const boost::filesystem::path &path)
+{
+	if (boost::filesystem::is_regular_file(path))	{return Node::Type::FILE;}
+	if (boost::filesystem::is_directory(path))   	{return Node::Type::DIRECTORY;}
+
+	return Node::Type::NODE;
+}
+
+boost::filesystem::path Node::get_path(PathType path_type)
+{
+	switch (path_type)
+	{
+		case ABSOLUTE : return get_absolute_path();
+		case RELATIVE : return get_relative_path();
+		default       : return Node::path;
+	}
+}
+
+boost::filesystem::path Node::get_absolute_path()
+{
+	return  boost::filesystem::canonical(Node::path);
+}
+
+boost::filesystem::path Node::get_relative_path()
+{
+	return  boost::filesystem::relative(Node::path);
+}
+
+void Node::set_path(const boost::filesystem::path & path)
+{
+	Node::path = path;
+};
+
+std::string Node::get_filename()
+{
+	return get_path().filename().string();
+}
+
+std::string Node::get_extention()
+{
+	return get_path().extension().string();
+}
+
+const Node::Type & Node::get_type()
+{
+	return Node::type;
+}
+
+std::string Node::get_type_str()
+{
+	switch (get_type())
+	{
+		case FILE      	: return "File";
+		case DIRECTORY	: return "Directory";
+		default       	: return "Node";
+	}
+}
+
+void Node::set_type(const Node::Type & type)
+{
+	Node::type = type;
+}
+
+time_t Node::get_creation_date_time()
+{
+	return boost::filesystem::creation_time(get_path());
+}
+
+time_t Node::get_last_write_date_time()
+{
+	return boost::filesystem::last_write_time(get_path());
+}
+
+std::string Node::get_creation_date_str()
+{
+	time_t time = get_creation_date_time();
+
+	struct tm local_time{};
+	localtime_r(&time, &local_time);
+	char date_buffer[256];
+	strftime(date_buffer, sizeof(date_buffer), "%d/%m/%Y %H:%M:%S", &local_time);
+
+	return std::string(date_buffer);
+}
+
+std::string Node::get_last_write_date_str()
+{
+	time_t time = get_last_write_date_time();
+
+	struct tm local_time{};
+	localtime_r(&time, &local_time);
+	char date_buffer[256];
+	strftime(date_buffer, sizeof(date_buffer), "%d/%m/%Y %H:%M:%S", &local_time);
+
+	return std::string(date_buffer);
+}
+
+bool Node::get_has_permissions(Node::PermissionOwner owner, const std::vector<PermissionType> & types)
+{
+	for(auto & type : types)
+	{
+		std::string permissions = std::bitset<9>(static_cast<int>(status(get_path()).permissions())).to_string();
+		int permission_i = type + (owner * 3);
+
+		if(permissions.at(permission_i) != '1') {return false ;}
+	}
+
+	return true;
+}
+
+bool Node::get_is_hidden()
+{
+	return get_path().leaf().c_str()[0] == '.';
+}
+
+std::string Node::to_string()
+{
+	return
+
+	"{\n"
+		"\tnode_type = " + get_type_str() + "\n"
+		"\tfile_name : " + get_path().filename().string() + "\n"
+		"\tsize = " + std::to_string(get_size()) + " byte(s)\n"
+		"\tcreation_date = " + get_creation_date_str() + "\n"
+		"\tlast_modification_date = " + get_last_write_date_str() + "\n"
+		"\tvisibility = " + ((get_is_hidden()) ? "hidden" : "visible") + "\n"
+	"}";
+}
