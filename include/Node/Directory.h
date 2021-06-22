260bd06323ebfcb48347fcddc188398c8a3bdc11
Elyas <el.idrissi_elyas@icloud.com>
2021-06-22 21:40:20 +0200

    add class 'Directory'

---
+++ include/Node/Directory.h
@@ -0,0 +1,13 @@
+//
+// Created by Elyas EL IDRISSI on 22/06/2021.
+//
+
+#ifndef SYNKER_DIRECTORY_H
+#define SYNKER_DIRECTORY_H
+
+class Directory
+{
+
+};
+
+#endif //SYNKER_DIRECTORY_H
---
+++ src/Node/Directory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Elyas EL IDRISSI on 22/06/2021.
+//
+
+#include "Node/Directory.h"
