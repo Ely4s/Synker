62b677c6283c1489425dfae7dc31c8bb9aa8de74
Elyas <el.idrissi_elyas@icloud.com>
2021-06-26 21:45:00 +0200

    add class 'Profiler'

---
+++ include/Profiler/Profiler.h
@@ -0,0 +1,13 @@
+//
+// Created by Elyas EL IDRISSI on 26/06/2021.
+//
+
+#ifndef SYNKER_PROFILER_H
+#define SYNKER_PROFILER_H
+
+class Profiler
+{
+
+};
+
+#endif //SYNKER_PROFILER_H
---
+++ src/Profiler/Profiler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Elyas EL IDRISSI on 26/06/2021.
+//
+
+#include "Profiler/Profiler.h"
