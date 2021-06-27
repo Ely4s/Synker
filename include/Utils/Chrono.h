c77764eb8d727b39ddb688187e05dbd65a5260dd
Elyas <el.idrissi_elyas@icloud.com>
2021-06-27 21:44:19 +0200

    add class 'Chrono' to namespace 'Utils' and initial implementation of it

--- include/Utils/Chrono.h
+++ include/Utils/Chrono.h
@@ -5,9 +5,35 @@
 #ifndef SYNKER_CHRONO_H
 #define SYNKER_CHRONO_H
 
+#include <chrono>
+
 class Chrono
 {
 
+public:
+
+	explicit Chrono(bool start = false);
+	~Chrono() = default;
+
+	void start();
+	void stop();
+
+	template<class T>
+	double get_result();
+	double get_result();
+
+private:
+
+	std::chrono::time_point<std::chrono::system_clock> m_start, m_end;
+
 };
+
+template<class T>
+double Chrono::get_result()
+{
+	std::chrono::duration<double, T> elapsed_time = m_end - m_start;
+
+	return elapsed_time.count();
+}
 
 #endif //SYNKER_CHRONO_H
--- src/Utils/Chrono.cpp
+++ src/Utils/Chrono.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "Utils/Chrono.h"
+
+Chrono::Chrono(bool start)
+{
+	if(start) Chrono::start();
+}
+
+void Chrono::start()
+{
+	m_start = std::chrono::system_clock::now();
+	m_end = m_start;
+}
+
+void Chrono::stop()
+{
+	m_end = std::chrono::system_clock::now();
+}
+
+double Chrono::get_result()
+{
+	return get_result<std::ratio<1>>();
+}
