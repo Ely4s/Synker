b99c907f81e41b5e69b6e66d6460298ce0b2a3a0
Elyas <el.idrissi_elyas@icloud.com>
2021-06-27 20:29:19 +0200

    add class 'Chrono' to namespace 'Utils'

---
+++ include/Utils/Chrono.h
@@ -0,0 +1,13 @@
+//
+// Created by Elyas EL IDRISSI on 27/06/2021.
+//
+
+#ifndef SYNKER_CHRONO_H
+#define SYNKER_CHRONO_H
+
+class Chrono
+{
+
+};
+
+#endif //SYNKER_CHRONO_H
---
+++ src/Utils/Chrono.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Elyas EL IDRISSI on 27/06/2021.
+//
+
+#include "Utils/Chrono.h"
