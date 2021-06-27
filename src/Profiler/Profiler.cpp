4810db0717d286bbb9534a9add79e49d1f822d64
Elyas <el.idrissi_elyas@icloud.com>
2021-06-27 20:26:09 +0200

    replace quotes in logs of class 'Profiler' by using the 'quoted()' method of module 'string' of namespace 'Utils'

--- src/Profiler/Profiler.cpp
+++ src/Profiler/Profiler.cpp
@@ -10,6 +10,7 @@
 #include "Node/Node.h"
 #include "Node/File.h"
 #include "Node/Directory.h"
+#include "Utils/string.h"
 
 #include "Profiler/Profiler.h"
 
@@ -21,7 +22,7 @@ std::pair<std::vector<boost::filesystem::path>, std::vector<boost::filesystem::p
 	//path not refers to a directory
 	if(Node::get_guessed_type(path) != Node::DIRECTORY)
 	{
-		std::string message = (boost::format("[profiling : \"%1%\"] invalid profiling root directory") % path.string()).str();
+		std::string message = (boost::format("[profiling : %1%] invalid profiling root directory") % path).str();
 		spdlog::error(message);
 		throw std::runtime_error(message);
 	}
@@ -31,17 +32,17 @@ std::pair<std::vector<boost::filesystem::path>, std::vector<boost::filesystem::p
 	//root_directory to profile hasn't user read and write permissions
 	if(!root_directory.get_has_permissions(Node::USER, {Node::READ, Node::WRITE}))
 	{
-		std::string message = (boost::format("[profiling : \"%1%\"] profiling root directory don't have read and write rights") % root_directory.get_path().string()).str();
+		std::string message = (boost::format("[profiling : %1%] profiling root directory don't have read and write rights") % root_directory.get_path()).str();
 		spdlog::error(message);
 		throw std::runtime_error(message);
 	}
 
-	spdlog::info("[profiling : \"{}\"] start of profiling", root_directory.get_path().string());
+	spdlog::info("[profiling : {}] start of profiling", Utils::quoted(root_directory.get_path().string()));
 
 	std::chrono::time_point<std::chrono::system_clock> start_time_profiling, end_time_profiling;
 	start_time_profiling = std::chrono::system_clock::now();
 
-	spdlog::info("[profiling : \"{}\"] start of profiling iteration", root_directory.get_path().string());
+	spdlog::info("[profiling : {}] start of profiling iteration", Utils::quoted(root_directory.get_path().string()));
 
 	std::chrono::time_point<std::chrono::system_clock> start_time_profiling_iteration, end_time_profiling_iteration;
 	start_time_profiling_iteration = std::chrono::system_clock::now();
@@ -57,21 +58,21 @@ std::pair<std::vector<boost::filesystem::path>, std::vector<boost::filesystem::p
 		{
 			unvalid_paths.push_back(node_it->path());
 			node_it.no_push();
-			spdlog::warn("[profiling : \"{}\"] can't add node path \"{}\" to the profile : {}", root_directory.get_path().string(), node_it->path().string(), "Permission denied");
+			spdlog::warn("[profiling : {}] can't add node path {} to the profile : {}", Utils::quoted(root_directory.get_path().string()), Utils::quoted(node_it->path().string()), "Permission denied");
 		}
 		//node refers to an undefined node type
 		if(Node::get_guessed_type(node_it->path()) == Node::NODE)
 		{
 			unvalid_paths.push_back(node_it->path());
 			node_it.no_push();
-			spdlog::warn("[profiling : \"{}\"] can't add node path \"{}\" to the profile : {}", root_directory.get_path().string(), node_it->path().string(), "Undefined node type");
+			spdlog::warn("[profiling : {}] can't add node path {} to the profile : {}", Utils::quoted(root_directory.get_path().string()), Utils::quoted(node_it->path().string()), "Undefined node type");
 		}
 		//node refers to a symlink
 		else if(Node::get_guessed_type(node_it->path()) == Node::SYMLINK)
 		{
 			unvalid_paths.push_back(node_it->path());
 			node_it.no_push();
-			spdlog::warn("[profiling : \"{}\"] can't add symlink file path \"{}\" to the profile : {}", root_directory.get_path().string(), node_it->path().string(), "Symlink not supported");
+			spdlog::warn("[profiling : {}] can't add symlink file path {} to the profile : {}", Utils::quoted(root_directory.get_path().string()), Utils::quoted(node_it->path().string()), "Symlink not supported");
 		}
 		//node refers to a file
 		else if(Node::get_guessed_type(node_it->path()) == Node::FILE)
@@ -82,14 +83,14 @@ std::pair<std::vector<boost::filesystem::path>, std::vector<boost::filesystem::p
 			if(file.get_has_permissions(Node::USER, {Node::READ, Node::WRITE}))
 			{
 				valid_paths.push_back(file.get_path());
-				spdlog::info("[profiling : \"{}\"] add file path \"{}\" to the profile", root_directory.get_path().string(), file.get_path().string());
+				spdlog::info("[profiling : {}] add file path {} to the profile", Utils::quoted(root_directory.get_path().string()), Utils::quoted(file.get_path().string()));
 			}
 			//file hasn't user read and write permissions
 			else
 			{
 				unvalid_paths.push_back(file.get_path());
 				node_it.no_push();
-				spdlog::warn("[profiling : \"{}\"] can't add file path \"{}\" to the profile : {}", root_directory.get_path().string(), file.get_path().string(), "Permission denied");
+				spdlog::warn("[profiling : {}] can't add file path {} to the profile : {}", Utils::quoted(root_directory.get_path().string()), Utils::quoted(file.get_path().string()), "Permission denied");
 			}
 		}
 		//node refers to a directory
@@ -105,13 +106,13 @@ std::pair<std::vector<boost::filesystem::path>, std::vector<boost::filesystem::p
 			if(ec_tmp == no_error)
 			{
 				valid_paths.push_back(directory.get_path());
-				spdlog::info("[profiling : \"{}\"] add directory path \"{}\" to the profile", root_directory.get_path().string(), directory.get_path().string());
+				spdlog::info("[profiling : {}] add directory path {} to the profile", Utils::quoted(root_directory.get_path().string()), Utils::quoted(directory.get_path().string()));
 			}
 			//can access to directory child nodes
 			else
 			{
 				unvalid_paths.push_back(directory.get_path());
-				spdlog::warn("[profiling : \"{}\"] can't add directory path \"{}\" to the profile : {}", root_directory.get_path().string(), directory.get_path().string(), ec_tmp.message());
+				spdlog::warn("[profiling : {}] can't add directory path {} to the profile : {}", Utils::quoted(root_directory.get_path().string()), Utils::quoted(directory.get_path().string()), ec_tmp.message());
 				//go to directory sibling node
 				node_it.no_push();
 			}
@@ -122,9 +123,9 @@ std::pair<std::vector<boost::filesystem::path>, std::vector<boost::filesystem::p
 
 	end_time_profiling_iteration = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed_seconds_profiling_iteration = end_time_profiling_iteration - start_time_profiling_iteration;
-	spdlog::info("[profiling : \"{}\"] end of profiling iteration ({} valid and {} unvalid node(s) found in {} second(s))", root_directory.get_path().string(), valid_paths.size(), unvalid_paths.size(), elapsed_seconds_profiling_iteration.count());
+	spdlog::info("[profiling : {}] end of profiling iteration ({} valid and {} unvalid node(s) found in {} second(s))", Utils::quoted(root_directory.get_path().string()), valid_paths.size(), unvalid_paths.size(), elapsed_seconds_profiling_iteration.count());
 
-	spdlog::info("[profiling : \"{}\"] start of duplicated path removal", root_directory.get_path().string());
+	spdlog::info("[profiling : {}] start of duplicated path removal", Utils::quoted(root_directory.get_path().string()));
 
 	std::chrono::time_point<std::chrono::system_clock> start_time_duplicated_removal, end_time_duplicated_removal;
 	start_time_duplicated_removal = std::chrono::system_clock::now();
@@ -146,12 +147,12 @@ std::pair<std::vector<boost::filesystem::path>, std::vector<boost::filesystem::p
 
 	end_time_duplicated_removal = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed_seconds_duplicated_removal = end_time_duplicated_removal - start_time_duplicated_removal;
-	spdlog::info("[profiling : \"{}\"] end of duplicated path removal ({} path(s) removed in {} second(s))", root_directory.get_path().string(), nbr_removed_path, elapsed_seconds_duplicated_removal.count());
+	spdlog::info("[profiling : {}] end of duplicated path removal ({} path(s) removed in {} second(s))", Utils::quoted(root_directory.get_path().string()), nbr_removed_path, elapsed_seconds_duplicated_removal.count());
 
 	end_time_profiling = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed_seconds_profiling = end_time_profiling - start_time_profiling;
 
-	spdlog::info("[profiling : \"{}\"] end of profiling (done in {} second(s))", root_directory.get_path().string(), elapsed_seconds_profiling.count());
+	spdlog::info("[profiling : {}] end of profiling (done in {} second(s))", Utils::quoted(root_directory.get_path().string()), elapsed_seconds_profiling.count());
 
 	return {valid_paths, unvalid_paths};
 }
